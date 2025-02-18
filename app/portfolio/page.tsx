549fd46b15548d0cca958da5a9be196820f3d5eb
tienld <tienld@bnksolution.com>
2025-02-18 10:30:00 +0700

    fix server side

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,10 +2,17 @@ import ControlPanel from '@/components/ControlPanel/ControlPanel';
 import InterView from '@/components/InterView/InterView';
 import About from '@/components/About/About';
 import Project from '@/components/Project/Project';
-import axiosClient from '@/services/api';
+import { API_ROUTES } from '@/shared/constants/router';
 
 export default async function PortfolioPage() {
-  const { result } = await axiosClient.apiGetProject();
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}${API_ROUTES.GET_PROJECT}`,
+    {
+      cache: 'no-store',
+    }
+  );
+
+  const { result } = await res.json();
 
   return (
     <div className="p-5 bg-white text-black dark:bg-black dark:text-white overflow-x-hidden">
